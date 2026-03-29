e8d2e3d3c188886a10d7943b587f5fe706a20529
starman-tech <gaudioso2008@gmail.com>
2026-03-29 21:28:01 +0200

    edsed

--- android/app/src/main/java/com/voxia/app/MainActivity.java
+++ android/app/src/main/java/com/voxia/app/MainActivity.java
@@ -47,4 +47,13 @@ public class MainActivity extends BridgeActivity {
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULL
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                );
+            } else {
+                v.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+            }
+        }
+    }
+}
