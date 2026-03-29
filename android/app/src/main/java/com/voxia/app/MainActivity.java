3feb1d99bf3e00322409ab22d276946c2505c7b1
starman-tech <gaudioso2008@gmail.com>
2026-03-29 21:26:54 +0200

    edse

--- android/app/src/main/java/com/voxia/app/MainActivity.java
+++ android/app/src/main/java/com/voxia/app/MainActivity.java
@@ -1,40 +1,50 @@
 package com.voxia.app;
 
-import com.getcapacitor.BridgeActivity;
 import android.os.Build;
+import android.os.Bundle;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
-public class MainActivity extends BridgeActivity {}
-// Colle ces deux méthodes dans ta MainActivity
-@Override
-protected void onWindowFocusChanged(boolean hasFocus) {
-    super.onWindowFocusChanged(hasFocus);
-    if (hasFocus) applySystemUI();
-}
+import com.getcapacitor.BridgeActivity;
 
-void applySystemUI() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        getWindow().setDecorFitsSystemWindows(false);
-        WindowInsetsController c = getWindow().getInsetsController();
-        if (c != null) {
-            // Masque STATUS BAR + NAV BAR si le flag nightMode est actif
-            // Sinon restaure
-            boolean night = getBridge().getWebView()
-                .evaluateJavascriptSync("String(window.__nightMode||false)").equals("true");
-            if (night) {
-                c.hide(WindowInsets.Type.systemBars());
-                c.setSystemBarsBehavior(
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-            } else {
-                c.show(WindowInsets.Type.systemBars());
-            }
-        }
-    } else {
-        View v = getWindow().getDecorView();
-        v.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
-}
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) applySystemUI();
+    }
+
+    private void applySystemUI() {
+        String nightMode = "false";
+        try {
+            nightMode = (String) getBridge().getWebView()
+                .evaluateJavascriptSync("String(window.__nightMode||false)");
+        } catch (Exception e) {
+            // ignore
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController c = getWindow().getInsetsController();
+            if (c != null) {
+                if ("true".equals(nightMode)) {
+                    c.hide(WindowInsets.Type.systemBars());
+                    c.setSystemBarsBehavior(
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+                } else {
+                    c.show(WindowInsets.Type.systemBars());
+                }
+            }
+        } else {
+            View v = getWindow().getDecorView();
+            if ("true".equals(nightMode)) {
+                v.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULL
